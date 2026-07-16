afad7ebd8d8e6953bc806b2bd51b071dd6ba2294
srinivasa senthil <srinivasasenthil.thirugnanam@trumpf.com>
2026-07-16 12:59:00 +0530

    Temparature days

---
+++ trainings26/Temparature.cs
@@ -0,0 +1,18 @@
+﻿namespace Trainings26 {
+   public class Temparature {
+      public int[] CoolerDays (int[] temperatures) {
+         int n = temperatures.Length;
+         int[] result = new int[n];
+         for (int i = 0; i < n; i++) {
+            result[i] = 0;
+            for (int j = i + 1; j < n; j++) {
+               if (temperatures[j] < temperatures[i]) {
+                  result[i] = j - i;
+                  break;
+               }
+            }
+         }
+         return result;
+      }
+   }
+}
