a9578b91c1f6354483db70f629a7d7e26f470a57
srinivasa senthil <srinivasasenthil.thirugnanam@trumpf.com>
2026-07-31 17:42:38 +0530

    Guessing game

---
+++ A02/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Console;
+public class A02 {
+   static void Main () {
+      bool playAgain = true;
+      while (playAgain) {
+         int randomValue =new Random().Next (1, 101), userValue;
+         do {
+            while (true) {
+               try {
+                  Write ("Enter a number between 1 and 100: ");
+                  userValue = int.Parse (ReadLine ());
+                  if (userValue < 1 || userValue > 100) {
+                     WriteLine ("Please enter a number between 1 and 100.");
+                     continue;
+                  }
+                  break;
+               } catch (FormatException) {
+                  WriteLine ("Please enter numbers only.");
+               }
+            }
+            if (userValue < randomValue) {
+               WriteLine ("Your guess is too low.");
+            } else if (userValue > randomValue) {
+               WriteLine ("Your guess is too high.");
+            } else {
+               WriteLine ("You guessed correctly!");
+            }
+         } while (userValue != randomValue);
+         Write ("Play again? (Y/N): ");
+         string choice = ReadLine ();
+         playAgain = choice == "Y" || choice == "y";
+      }
+   }
+}
