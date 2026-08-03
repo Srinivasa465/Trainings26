a0c36be46b81baa412b78adace87e6f64e1f2cca
srinivasa senthil <srinivasasenthil.thirugnanam@trumpf.com>
2026-08-03 14:53:50 +0530

    review changes

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,34 +1,45 @@
-﻿using static System.Console;
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch - July 2026.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program to guess a random number between a range.
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
 public class A02 {
    static void Main () {
       bool playAgain = true;
       while (playAgain) {
-         int randomValue =new Random().Next (1, 101), userValue;
+         int randomValue = new Random ().Next (1, 101), userValue;
          do {
             while (true) {
-               try {
-                  Write ("Enter a number between 1 and 100: ");
-                  userValue = int.Parse (ReadLine ());
-                  if (userValue < 1 || userValue > 100) {
-                     WriteLine ("Please enter a number between 1 and 100.");
-                     continue;
-                  }
-                  break;
-               } catch (FormatException) {
-                  WriteLine ("Please enter numbers only.");
+               Write ("Enter a number between 1 and 100: ");
+               if (!int.TryParse (Console.ReadLine (), out userValue)) {
+                  WriteLine ("Please enter a valid number:");
+                  continue;
                }
+               if (userValue < 1 || userValue > 100) {
+                  WriteLine ("Please enter a number between 1 and 100.");
+                  continue;
+               }
+               break;
             }
-            if (userValue < randomValue) {
-               WriteLine ("Your guess is too low.");
-            } else if (userValue > randomValue) {
-               WriteLine ("Your guess is too high.");
-            } else {
-               WriteLine ("You guessed correctly!");
+            switch (userValue) {
+               case < 101 when userValue < randomValue:
+                  WriteLine ("Your guess is too low.");
+                  break;
+               case <= 100 when userValue > randomValue:
+                  WriteLine ("Your guess is too high.");
+                  break;
+               default:
+                  WriteLine ("You guessed correctly!");
+                  break;
             }
          } while (userValue != randomValue);
-         Write ("Play again? (Y/N): ");
-         string choice = ReadLine ();
-         playAgain = choice == "Y" || choice == "y";
+         WriteLine ("Play again? (Y/N): ");
+         ConsoleKeyInfo key = ReadKey ();
+         WriteLine ();
+         playAgain = key.Key == ConsoleKey.Y;
       }
    }
 }
