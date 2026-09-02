4c15a20ac0a34d31a1345ce5e5d79704bd26d1c8
srinivasa senthil <srinivasasenthil.thirugnanam@trumpf.com>
2026-09-02 14:32:08 +0530

    A03: Updates

--- A03/Program.cs
+++ A03/Program.cs
@@ -12,18 +12,23 @@ namespace A03;
 public class Program {
    static void Main () {
       char[] letters = { 'u', 'x', 'a', 'l', 't', 'n', 'e' };
-      var validwords = File.ReadAllLines ("C:/Work/Words.txt").
-         Where (w => w.Contains (letters[0]) && w.All (l => letters.Contains (l)) && w.Length >= 4);
-      foreach (var word in validwords.OrderByDescending (word => word.Length)) {
+      var validwords = File.ReadAllLines ("C:/Work/Words.txt")
+         .Where (w => w.Length >= 4 && w.Contains (letters[0]) && w.All (l => letters.Contains (l)));
+      var scoredWords = validwords.Select (word => {
          bool containsAllLetters = letters.All (letter => word.Contains (letter));
          int point = word.Length > 4 ? word.Length : 1;
-         if (containsAllLetters) {
-            point = 15;
+         if (containsAllLetters)
+            point += 7;
+         return (word, point, containsAllLetters);
+      });
+      foreach (var item in scoredWords.OrderByDescending (x => x.point).ThenBy (x => x.word)) {
+         if (item.containsAllLetters) {
             ForegroundColor = ConsoleColor.DarkGreen;
-            WriteLine ($"{point}. {word.ToUpper()}");
+            WriteLine ($"{item.point,2}. {item.word.ToUpper ()}");
             ResetColor ();
-         } else WriteLine ($"{point,2}. {word.ToUpper()}");
+         } else {
+            WriteLine ($"{item.point,2}. {item.word.ToUpper ()}");
+         }
       }
    }
 }
-
